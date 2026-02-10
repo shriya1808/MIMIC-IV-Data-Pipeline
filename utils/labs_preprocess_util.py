cb15a2d70f5f7d6631fff0286b867adf0c262ffa
Kartik Nagaraj Nayak <kartiknnayak2000@gmail.com>
2026-02-10 17:09:30 +0100

    CAD changes

--- utils/labs_preprocess_util.py
+++ utils/labs_preprocess_util.py
@@ -7,8 +7,8 @@ from collections import defaultdict
 from typing import Union, List, Tuple
 import pandas as pd
 import numpy as np
-if not os.path.exists("./dataCKD/temp"):
-    os.makedirs("./dataCKD/temp")
+if not os.path.exists("./dataCAD/temp"):
+    os.makedirs("./dataCAD/temp")
 
 def hadm_imputer(
     charttime: pd._libs.tslibs.timestamps.Timestamp,
